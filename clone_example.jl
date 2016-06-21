ad7e261e9e1c2f3e6a0a02590774d52d5030ccbe
Tony Kelman <tony@kelman.net>
2016-06-21 15:18:36 +0000

    Add my https push remote manually

--- clone_example.jl
+++ clone_example.jl
@@ -8,18 +8,22 @@ function clone_or_pull(url; pkg=false)
         if isdir(Pkg.dir(name))
             run(setenv(`git pull`; dir=Pkg.dir(name)))
         else
-            Pkg.clone(url)
+            Pkg.clone(string("https://", url))
             symlink(Pkg.dir(name), dir)
+            # for my own pushing convenience because ssh is annoying
+            run(setenv(`git remote add tkelman https://tkelman@$url`; dir=dir))
         end
     else
         if isdir(dir)
             run(setenv(`git pull`; dir=dir))
         else
-            run(`git clone $url`)
+            run(`git clone https://$url`)
+            # for my own pushing convenience because ssh is annoying
+            run(setenv(`git remote add tkelman https://tkelman@$url`; dir=dir))
         end
     end
 end
 cd(dirname(dirname(@__FILE__))) do
-    clone_or_pull("https://github.com/tkelman/Rmath-julia")
-    clone_or_pull("https://github.com/tkelman/Rmath.jl"; pkg=true)
+    clone_or_pull("github.com/tkelman/Rmath-julia")
+    clone_or_pull("github.com/tkelman/Rmath.jl"; pkg=true)
 end
