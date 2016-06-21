4f207108546a9d943cda54c719ddae035a0769eb
tony.kelman <tony.kelman@juliacomputing.com>
2016-06-21 13:37:18 +0000

    Use Pkg.dir on Rmath.jl to avoid messing with LOAD_PATH

--- clone_example.jl
+++ clone_example.jl
@@ -1,14 +1,24 @@
 #!/usr/bin/env julia
 # separate file because I can't get an equivalent of @__FILE__ to work in IJulia
-function clone_or_pull(url)
+# TODO file an issue about that and replace this line with a link to it
+function clone_or_pull(url; pkg=false)
     dir = basename(url)
-    if isdir(dir)
-        run(setenv(`git pull`; dir=dir))
+    if pkg
+        name = splitext(dir)[1]
+        if isdir(Pkg.dir(name))
+            run(setenv(`git pull`; dir=Pkg.dir(name)))
+        else
+            Pkg.clone(url)
+        end
     else
-        run(`git clone $url`)
+        if isdir(dir)
+            run(setenv(`git pull`; dir=dir))
+        else
+            run(`git clone $url`)
+        end
     end
 end
 cd(dirname(dirname(@__FILE__))) do
     clone_or_pull("https://github.com/tkelman/Rmath-julia")
-    clone_or_pull("https://github.com/tkelman/Rmath.jl")
+    clone_or_pull("https://github.com/tkelman/Rmath.jl"; pkg=true)
 end
